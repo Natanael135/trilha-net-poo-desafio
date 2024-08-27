76671be08374dde8d9738f5b86fa125ef6ee0cc6
Natanael Melo <natanaelmelo.dev@gmail.com>
2024-08-27 16:33:25 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("0101", "Iphone 14", "imei:0101-0101", 256);
+Nokia nokia = new Nokia("0202", "Nokia Tijolao", "imei:0202-0202", 4);
+
+//teste iphone
+Console.WriteLine("Teste do Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("whatsapp");
+
+
+//teste nokia
+Console.WriteLine("\nTeste do Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("facebook");
+
+
